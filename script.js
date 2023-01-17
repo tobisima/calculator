70a2e8b76736c698d4be87b4809d92283711f40a
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2023-01-18 01:05:26 +1000

    Make the calculator work

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const screen = document.querySelector('.screen');
+const numButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('.equals');
+let screenValueA = '';
+let screenValueB = '';
+let operatorValue;      
+
 function add(a, b) {
     return a + b;
 }
@@ -15,6 +23,32 @@ function divide(a, b) {
 }
  
 function operate(operator, a, b) {
-  return operator(a, b);     
+  screen.textContent = operator(+a, +b);
 }
 
+numButtons.forEach(numButton =>
+  numButton.onclick = populateDisplay);
+  
+function populateDisplay(e) {
+  if (!operatorValue) {
+    screen.textContent += e.target.textContent;
+  } else {  
+    screenValueB += e.target.textContent;
+    screen.textContent = screenValueB;
+  }
+}
+      
+operatorButtons.forEach(operatorButton =>
+  operatorButton.onclick = assignOperator);
+
+  function assignOperator(e) {
+  screenValueA = screen.textContent;
+  operatorValue = e.target.id;
+}
+  
+equalsButton.onclick = showResult; 
+
+function showResult() {
+  operate(window[operatorValue], screenValueA, screenValueB);
+  screenValueB = '';  
+}
